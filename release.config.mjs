04ae9e2332c1f4872f73822b9f9dcc819ae96f78
Midhath Ahmed <mdhthahmd@gmail.com>
2026-08-24 13:15:06 +0000

    ci: enable npm provenance

--- release.config.mjs
+++ release.config.mjs
@@ -14,7 +14,7 @@ export default {
 				prepareCmd:
 					"pnpm version ${nextRelease.version} --no-git-tag-version --no-git-checks",
 				publishCmd:
-					"pnpm publish --tag ${nextRelease.channel || 'latest'} --no-git-checks",
+					"pnpm publish --provenance --tag ${nextRelease.channel || 'latest'} --no-git-checks",
 				addChannelCmd:
 					"pnpm dist-tag add $(pnpm pkg get name --json | tr -d '\"')@${nextRelease.version} ${nextRelease.channel}",
 			},
